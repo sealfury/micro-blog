b52a149a05544d84030a6e3b60afe19a8b1a662f
sealfury <willspecker@gmail.com>
2021-09-21 17:44:22 +0200

    updates routes for event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -15,15 +15,18 @@ app.post('/events', (req, res) => {
   axios.post('http://posts-clusterip-srv:4000/events', event).catch(err => {
     console.log(err.message)
   })
-  // axios.post('http://localhost:4001/events', event).catch(err => {
-  //   console.log(err.message)
-  // })
-  // axios.post('http://localhost:4002/events', event).catch(err => {
-  //   console.log(err.message)
-  // })
-  // axios.post('http://localhost:4003/events', event).catch(err => {
-  //   console.log(err.message)
-  // })
+  // Comments service
+  axios.post('http://comments-srv:4001/events', event).catch(err => {
+    console.log(err.message)
+  })
+  // Query service
+  axios.post('http://query-srv:4002/events', event).catch(err => {
+    console.log(err.message)
+  })
+  // Moderation service
+  axios.post('http://moderation-srv:4003/events', event).catch(err => {
+    console.log(err.message)
+  })
 
   res.send({ status: 'OK' })
 })
